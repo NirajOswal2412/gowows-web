1a4b0ddf39e7e9542e3b65074f6f56baf5e68fc8
NirajOswal2412 <161004965+NirajOswal2412@users.noreply.github.com>
2025-06-13 10:45:04 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:7860";
-export const BASE_URL = "https://test-backend-gowowschat-252628919239.asia-south1.run.app"
+export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "https://test-backend-gowowschat-252628919239.asia-south1.run.app"
